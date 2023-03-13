ffff617fb9f9604d1f10174cec18886337f7d52a
Ix procopios <enprocopios@gmail.com>
2023-03-12 18:40:19 -0700

    Initialize 06-example-starter-Auth-pt2

---
+++ 06-Auth-pt2/client/src/components/Authentication.js
@@ -0,0 +1,94 @@
+import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import styled from "styled-components";
+import { useFormik } from "formik"
+import * as yup from "yup"
+
+
+function Authentication({updateUser}) {
+  const [signUp, setSignUp] = useState(false)
+  const history = useHistory()
+
+  const handleClick = () => setSignUp((signUp) => !signUp)
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Please enter a user name"),
+    email: yup.string().email()
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      name:'',
+      email:'',
+      password:''
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+        fetch(signUp?'/signup':'/login',{
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values, null, 2),
+        })
+        .then(res => {
+          if(res.ok){
+            res.json().then(user => {
+              updateUser(user)
+              history.push('/')
+            })
+          } else {
+            res.json().then(console.log)
+          }
+        })
+       
+    },
+  })
+
+    return (
+        <> 
+        <h2 style={{color:'red'}}> {formik.errors.name}</h2>
+        <h2>Please Log in or Sign up!</h2>
+        <h2>{signUp?'Already a member?':'Not a member?'}</h2>
+        <button onClick={handleClick}>{signUp?'Log In!':'Register now!'}</button>
+        <Form onSubmit={formik.handleSubmit}>
+        <label>
+          Username
+          </label>
+        <input type='text' name='name' value={formik.values.name} onChange={formik.handleChange} />
+        {signUp&&(
+          <>
+          <label>
+          Email
+          </label>
+          <input type='text' name='email' value={formik.values.email} onChange={formik.handleChange} />
+           <label>
+           Password
+           </label>
+           <input type='password' name='password' value={formik.values.password} onChange={formik.handleChange} />
+           </>
+        )}
+        <input type='submit' value={signUp?'Sign Up!':'Log In!'} />
+      </Form>
+        </>
+    )
+}
+
+export default Authentication
+
+export const Form = styled.form`
+display:flex;
+flex-direction:column;
+width: 400px;
+margin:auto;
+font-family:Arial;
+font-size:30px;
+input[type=submit]{
+  background-color:#42ddf5;
+  color: white;
+  height:40px;
+  font-family:Arial;
+  font-size:30px;
+  margin-top:10px;
+  margin-bottom:10px;
+}
+`
---
+++ 06-Auth-pt2/client/src/components/Navigation.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+function Navigation({updateUser}) {
+ const [menu, setMenu] = useState(false)
+ const history = useHistory()
+
+ const handleLogout = () => {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then(res => {
+      if(res.ok){
+          updateUser(null)
+          history.push('/authentication')
+      }
+    })
+ }
+
+    return (
+        <Nav> 
+         <NavH1>Flatiron Theater Company</NavH1>
+         <Menu>
+           {!menu?
+           <div onClick={() => setMenu(!menu)}>
+             <GiHamburgerMenu size={30}/> 
+           </div>:
+           <ul>
+            <li onClick={() => setMenu(!menu)}>x</li>
+            <li><Link to='/productions/new'>New Production</Link></li>
+            <li><Link to='/'> Home</Link></li>
+            <li><Link to='/authentication'> Login/Signup</Link></li>
+            <li onClick={handleLogout}> Logout </li>
+           </ul>
+           }
+         </Menu>
+
+        </Nav>
+    )
+}
+
+export default Navigation
+
+
+const NavH1 = styled.h1`
+font-family: 'Splash', cursive;
+`
+const Nav = styled.div`
+  display: flex;
+  justify-content:space-between;
+  
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  a{
+    text-decoration: none;
+    color:white;
+    font-family:Arial;
+  }
+  a:hover{
+    color:pink
+  }
+  ul{
+    list-style:none;
+  }
+  
+`;
