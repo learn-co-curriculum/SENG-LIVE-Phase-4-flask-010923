4aa9a2602ac0d7340a8d99e80f209160db5b48e8
Ix procopios <enprocopios@gmail.com>
2023-03-12 18:40:16 -0700

    Initialize 01-example-starter-intro-to-flask

---
+++ 01-intro-to-flask/server/models.py
@@ -0,0 +1,15 @@
+# 📚 Review With Students:
+    # Review models
+    # Review MVC
+#SQLAlchemy import
+from flask_sqlalchemy import SQLAlchemy
+
+# 📚 Review With Students:
+    # What SQLAlchemy() is replacing from SQLAlchemy in phase 3
+     
+db = SQLAlchemy()
+# 1. ✅ Create a Production Model
+	# tablename = 'Productions'
+	# Columns:
+        # title: string, genre: string, budget:float, image:string,director: string, description:string, ongoing:boolean, created_at:date time, updated_at: date time 
+# 2. ✅ navigate to app.py
---
+++ 01-intro-to-flask/server/seed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# 📚 Review With Students:
+    # Seeding 
+# 5. ✅ Imports
+    # app from app
+    # db and Production from models
+
+# 6. ✅ Initialize the SQLAlchemy instance with `db.init_app(app)`
+
+
+# 7. ✅ Create application context `with app.app_context():`
+    # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
+
+# 8.✅ Create a query to delete all existing records from Production    
+   
+# 9.✅ Create some seeds for production and commit them to the database. 
+# 10.✅ Run in terminal:
+    # `python seed.py`
+# 11.✅ navigate to debug.py
+    
