bbcb777627ab51e9485bdfdf6c9ffdc98771e2f7
Thomas Wolf <thomasrussellwolf@gmail.com>
2023-09-12 17:13:07 -0500

    removed step 6

--- 01-intro-to-flask/server/seed.py
+++ 01-intro-to-flask/server/seed.py
@@ -5,7 +5,6 @@
     # app from app
     # db and Production from models
 
-# 6. ✅ Initialize the SQLAlchemy instance with `db.init_app(app)`
 
 
 # 7. ✅ Create application context `with app.app_context():`
