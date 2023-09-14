a203f77b1eb4ee5e678e6584c513a403d4e02bea
Princeton J. Rose <princetonjrose@gmail.com>
2023-09-14 15:22:18 -0400

    updated pre-lecture code

--- 03-REST-API-Flask-pt2/server/app.py
+++ 03-REST-API-Flask-pt2/server/app.py
@@ -2,18 +2,32 @@
 # 📚 Review With Students:
     # REST
     # Status codes
+        # Most common response codes
+            # 200 = ok
+            # 201 = created ( post )
+            # 204 = no content ( delete )
+            # 404 = not found
+            # 401 = unauthorized ( login )
+            # 422 = unprocessable entity ( validation errors )
     # Error handling 
-# Set up:
-    # cd into server and run the following in the terminal
-    # export FLASK_APP=app.py
-    # export FLASK_RUN_PORT=5000
-    # flask db init
-    # flask db revision --autogenerate -m'Create tables' 
-    # flask db upgrade 
-    # python seed.py
+# Set Up:
+    # In Terminal, `cd` into `server` and run the following:
+        # export FLASK_APP=app.py
+        # export FLASK_RUN_PORT=5555
+        # export FLASK_DEBUG=1
+        # flask db init
+        # flask db revision --autogenerate -m 'Create tables' 
+        # flask db upgrade 
+        # python seed.py
+        # flask shell
+
+# Stretch Goal: Talk about redirect
+
 from flask import Flask, request, make_response, abort
 from flask_migrate import Migrate
 
+# ✅ Import `Api` and `Resource` from `flask_restful`
+from flask_restful import Api, Resource
 from flask_restful import Api, Resource
 
 # 1.✅ Import NotFound from werkzeug.exceptions for error handling
@@ -29,57 +43,32 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 api = Api(app)
 
-class Productions(Resource):
-    def get(self):
-        production_list = [p.to_dict() for p in Production.query.all()]
-        response = make_response(
-            production_list,
-            200,
-        )
 
-        return response
+# 2.✅ use the @app.errorhandler() decorator to handle Not Found
+    # 2.1 Create the decorator and pass it NotFound
+    # 2.2 Use make_response to create a response with a message and the status 404
+    # 2.3 return the response
 
-    def post(self):
-        request_json = request.get_json()
-       
-        new_production = Production(
-            title=request_json['title'],
-            genre=request_json['genre'],
-            budget=request_json['budget'],
-            image=request_json['image'],
-            director=request_json['director'],
-            description=request_json['description'],
-            ongoing=request_json['ongoing']
-        )
-
-        db.session.add(new_production)
-        db.session.commit()
-
-        response_dict = new_production.to_dict()
-
-        response = make_response(
-            response_dict,
-            201,
-        )
-        return response
-api.add_resource(Productions, '/productions')
-
-
-class ProductionByID(Resource):
-    def get(self,id):
-        production = Production.query.filter_by(id=id).first()
-# 3.✅ If a production is not found raise the NotFound exception
+class Productions ( Resource ) :
+    def get ( self ) :
+        return make_response( Production.all(), 200 )
     
-        production_dict = production.to_dict()
-        response = make_response(
-            production_dict,
-            200
-        )
-        
-        return response
+
+api.add_resource( Productions, '/productions', endpoint = 'productions' )
+
+class ProductionById ( Resource ) :
+    def get ( self, id ) :
+        prod = Production.find_by_id( id )
+        if prod :
+            return make_response( prod.to_dict_with_cast(), 200 )
+        else :
+            return make_response( { 'errors': ['Production was not found.'] }, 404 )
+
+api.add_resource( ProductionById, '/productions/<int:id>', endpoint = 'production' )
+
+# 3.✅ If a production is not found raise the NotFound exception
 
 # 4.✅ Patch
     # 4.1 Create a patch method that takes self and id
@@ -88,55 +77,44 @@ class ProductionByID(Resource):
     # 4.4 Loop through the request.form object and update the productions attributes. Note: Be cautions of the data types to avoid errors.
     # 4.5 add and commit the updated production 
     # 4.6 Create and return the response
-  
+
 # 5.✅ Delete
     # 5.1 Create a delete method, pass it self and the id
     # 5.2 Query the Production 
     # 5.3 If the production is not found raise the NotFound exception
     # 5.4 delete the production and commit 
     # 5.5 create a response with the status of 204 and return the response 
-  
 
-   
+
+
 api.add_resource(ProductionByID, '/productions/<int:id>')
 
-# 2.✅ use the @app.errorhandler() decorator to handle Not Found
-    # 2.1 Create the decorator and pass it NotFound
-    # 2.2 Use make_response to create a response with a message and the status 404
-    # 2.3 return t he response
+@app.route( '/cast_members', methods = [ 'GET', 'POST' ] )
+def cast_members ( ) :
+    if request.method == 'GET' :
+        return make_response( CastMember.all(), 200 )
+    
+    elif request.method == 'POST' :
+        rq = request.get_json()
+        new_cm = CastMember(
+            name = rq[ 'name' ],
+            role = rq[ 'role' ]
+        )
+        db.session.add( new_cm )
+        db.session.commit()
+        return make_response( new_cm.to_dict() , 201 )
+
+
+@app.route( '/cast_members/<int:id>', methods = [ 'GET' ] )
+def cast_member ( id ) :
+    cm = CastMember.find_by_id( id )
+    if cm :
+        if request.method == 'GET' :
+            return make_response( cm.to_dict_with_prod(), 200 )
+    else :
+        return make_response( { 'errors': ['Cast member was not found.'] }, 404 )
 
 
 # To run the file as a script
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
-
-#Student Exercises 
-class CastMembers(Resource):
-    def get(self):
-        cast_members_list = [cast_member.to_dict() for cast_member in CastMember.query.all()]
-    
-        response = make_response(
-            cast_members_list,
-            200
-        )
-        return response
-
-    def post(self):
-        request_json = request.get_json()
-        new_cast = CastMember(
-            name=request_json['name'],
-            role=request_json['role'],
-            production_id=request_json['production_id']
-        )
-        db.session.add(new_cast)
-        db.session.commit()
-
-        response_dict = new_cast.to_dict()
-        
-        response = make_response(
-            response_dict,
-            201
-        )
-        return response
-
-api.add_resource(CastMembers, '/cast_members')
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- 03-REST-API-Flask-pt2/server/models.py
+++ 03-REST-API-Flask-pt2/server/models.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# ✅ Import `SerializerMixin` from `sqlalchemy_serializer`
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
+
+# ✅ Pass `SerializerMixin` to `Productions`
 class Production(db.Model, SerializerMixin):
     __tablename__ = 'productions'
 
@@ -14,31 +17,84 @@ class Production(db.Model, SerializerMixin):
     director = db.Column(db.String)
     description = db.Column(db.String)
     ongoing = db.Column(db.Boolean)
+
+    # YOU SHOULD ALWAYS HAVE THESE 2 COLUMNS ON YOUR MODELS!!!! 🫡
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # Talk more about how the relationship method works! 🧑‍🏫
     cast_members = db.relationship('CastMember', backref='production')
 
-    serialize_rules = ('-cast_members.production',)
+
+    @classmethod
+    def all ( cls ) :
+        return [ prod.to_dict() for prod in Production.query.all() ]
+    
+    @classmethod
+    def find_by_id ( cls, id ) :
+        return Production.query.filter_by( id = id ).first()
+
+    def to_dict ( self ) :
+        return {
+            'id': self.id,
+            'title': self.title,
+            'budget': self.budget,
+            'genre': self.genre,
+            'image': self.image,
+            'director': self.director,
+            'description': self.description,
+            'ongoing': self.ongoing
+        }
+
+    # ✅ Create a serialize rule that will help add our `cast_members` to the response.
+    # serialize_rules = ('-cast_members.production',)
+    
+    def to_dict_with_cast ( self ) :
+        prod = self.to_dict()
+        prod[ 'cast' ] = [ cm.to_dict() for cm in self.cast_members ]
+        return prod
 
     def __repr__(self):
         return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
 
 
+# ✅ Pass `SerializerMixin` to `CastMember`
 class CastMember(db.Model, SerializerMixin):
     __tablename__ = 'cast_members'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     role = db.Column(db.String)
+
+    # YOU SHOULD ALWAYS HAVE THESE 2 COLUMNS ON YOUR MODELS!!!! 🫡
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
+
+    @classmethod
+    def all ( cls ) :
+        return [ cm.to_dict() for cm in CastMember.query.all() ]
     
+    @classmethod
+    def find_by_id ( cls, id ) :
+        return CastMember.query.filter_by( id = id ).first()
+        # return CastMember.query.filter( CastMember.id == id ).first()
 
-    serialize_rules = ('-production.cast_member',)
+    def to_dict ( self ) :
+        return {
+            'id': self.id,
+            'name': self.name,
+            'role': self.role
+        }
+    
+    # ✅ Create a serialize rule that will help add our `production` to the response.
+    # serialize_rules = ('-production.cast_member',)
 
+    def to_dict_with_prod ( self ) :
+        cm = self.to_dict()
+        cm[ 'production' ] = self.production.to_dict()
+        return cm
+    
     def __repr__(self):
-        return f'<Production Name:{self.name}, Role:{self.role}'
-
+        return f'<CastMember Name:{self.name}, Role:{self.role}'
