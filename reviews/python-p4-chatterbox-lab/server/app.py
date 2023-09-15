79a0420d4aa7b0b751a86181148771dda3a2dea9
Thomas Wolf <thomasrussellwolf@gmail.com>
2023-09-15 14:37:49 -0500

    review done

--- reviews/python-p4-chatterbox-lab/server/app.py
+++ reviews/python-p4-chatterbox-lab/server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        all_messages = Message.query.order_by(Message.created_at).all()
 
-@app.route('/messages/<int:id>')
+        json_messages = [message.to_dict() for message in all_messages]
+
+        return make_response(json_messages, 200)
+    elif request.method == "POST":
+        rq = request.get_json()
+        message = Message(
+            body = rq.get('body'),
+            username = rq.get('username')
+        )
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(), 200)
+    else:
+        return make_response({}, 500)
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    
+    if message:
+        if request.method == "PATCH":
+            rq = request.get_json()
+            message.body = rq.get('body')
+            
+            db.session.add(message)
+            db.session.commit()
+
+            return make_response(message.to_dict(), 200)
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+            return make_response({}, 200)
+        else:
+            return f'I got method {request.method}, and ID {id}'
+    else:
+        return make_response({}, 404)
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000, debug=True)
--- reviews/python-p4-chatterbox-lab/server/models.py
+++ reviews/python-p4-chatterbox-lab/server/models.py
@@ -12,3 +12,9 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    # def to_dict(): <- this is put here by Serializer
