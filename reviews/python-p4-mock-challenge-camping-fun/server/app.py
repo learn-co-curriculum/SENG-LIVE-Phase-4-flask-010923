8a8bd61d9996c664aff511811d88fb8a5b0188b4
Thomas Wolf <thomasrussellwolf@gmail.com>
2023-09-20 09:28:03 -0500

    init review

---
+++ reviews/python-p4-mock-challenge-camping-fun/server/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from models import db, Activity, Camper, Signup
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+
+@app.route('/')
+def home():
+    return ''
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
---
+++ reviews/python-p4-mock-challenge-camping-fun/server/models.py
@@ -0,0 +1,69 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Activity(db.Model, SerializerMixin):
+    __tablename__ = 'activities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    difficulty = db.Column(db.Integer)
+
+    # Add relationship
+    
+    # Add serialization rules
+    
+    def __repr__(self):
+        return f'<Activity {self.id}: {self.name}>'
+
+
+class Camper(db.Model, SerializerMixin):
+    __tablename__ = 'campers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+
+    # Add relationship
+    
+    # Add serialization rules
+    
+    # Add validation
+    
+    
+    def __repr__(self):
+        return f'<Camper {self.id}: {self.name}>'
+
+
+class Signup(db.Model, SerializerMixin):
+    __tablename__ = 'signups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+
+    # Add relationships
+    
+    # Add serialization rules
+    
+    # Add validation
+    
+    def __repr__(self):
+        return f'<Signup {self.id}>'
+
+
+# add any models you may need.
